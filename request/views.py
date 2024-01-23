591fab325c4a9818661610184d36b4d38e9031dd
yamashita <ymstsny12@gmail.com>
2024-01-23 14:29:31 +0900

    終了

--- request/views.py
+++ request/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 import requests
 
-url = "https://weather.tsukumijima.net/"
-
 # Create your views here.
 
 def index(request):
@@ -10,4 +8,28 @@ def index(request):
     return render(request, 'request/index.html',context)
 
 def get_weather_data(request):
-    return render(request, 'request/weather.html')
+    if request.method == 'POST':
+        location = request.POST['location']
+
+    param = {
+            "city" : location
+        }
+    
+    url = "https://weather.tsukumijima.net/api/forecast"
+    response = requests.get(url,params=param)
+    data =  response.json()
+
+    today = data["forecasts"][0]
+    date = "<" + today["date"] + ">"
+    detail = (f'今日は{today["detail"]["weather"]}で、{today["detail"]["wind"]}の風')
+    min_temperature = (f'最低気温は{today["temperature"]["min"]["celsius"]}℃')
+    max_temperature = (f'最高気温は{today["temperature"]["max"]["celsius"]}℃')
+
+    context = {
+        "date": date,
+        "detail" : detail,
+        "min_temperature" : min_temperature,
+        "max_temperature" : max_temperature,
+    }
+
+    return render(request, 'request/weather.html',context)
